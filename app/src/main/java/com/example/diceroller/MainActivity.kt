52310861320cbb1fafc1a0d82dde7172f24407fb
orlyc126 <orcohen@lightrix.com>
2025-09-04 14:52:50 +0300

    Fixed the coding style issues

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,23 +32,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 @Preview
-fun DiceRollerApp(){
+fun DiceRollerApp() {
     DiceWithButtonAndImage()
 }
 
 @Composable
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize(Alignment.Center))
-{
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { /*TODO*/ })
-        {
-            Text(stringResource(R.string.roll))
-        }
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
