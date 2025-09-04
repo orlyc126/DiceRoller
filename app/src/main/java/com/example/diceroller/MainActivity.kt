07b4fac49eeb586b02c727a5235ba81dd0bd87bf
orlyc126 <orcohen@lightrix.com>
2025-09-04 11:47:03 +0300

    added button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -22,11 +32,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 @Preview
-fun DiceRollerApp() {
+fun DiceRollerApp(){
     DiceWithButtonAndImage()
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
-
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center))
+{
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ })
+        {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
